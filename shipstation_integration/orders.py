9a75349cfe5b44a25cb0f8aeb514723de3a3c68a
Avunu LLC <mail@avu.nu>
2026-07-02 16:29:21 -0400

    feat: log requested shipping service

--- shipstation_integration/orders.py
+++ shipstation_integration/orders.py
@@ -209,6 +209,7 @@ def create_erpnext_order(
             "status": status,
             "shipstation_store_name": store.store_name,
             "shipstation_order_id": order.order_id,
+            "shipstation_requested_shipping_service": getattr(order, "requested_shipping_service", None),
             "shipstation_customer_notes": getattr(order, "customer_notes", None),
             "shipstation_internal_notes": getattr(order, "internal_notes", None),
             "marketplace": store.marketplace_name,
